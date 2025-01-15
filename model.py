6585831c774db59d737a91eb962a02b28496c5d2
gkmocastro <gkmocastro@gmail.com>
2025-01-15 17:35:55 -0500

    done loading rgb pretrained weights, needs to load depth weights on fusion model

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import lightnet as ln
 import functools
+import torch
 from torch import nn
+from utils import rename_state_dict
+from remapping_rules import remap_rgbd_15_fusion_model
 
 def model_builder(num_classes, model_type="rgb", fuse_layer=16):
     """ 
@@ -12,7 +15,7 @@ def model_builder(num_classes, model_type="rgb", fuse_layer=16):
     if model_type=="rgb":
         detection_model = ln.models.YoloV2(num_classes)
         detection_model.load('weights/yolo-pretrained_darknet.pt', strict=False)
-
+        return detection_model
     elif model_type=="depth":
         detection_model = ln.models.YoloV2(num_classes)
         detection_model.load('weights/yolo-pretrained_darknet.pt', strict=False)
@@ -25,9 +28,20 @@ def model_builder(num_classes, model_type="rgb", fuse_layer=16):
                                            activation=functools.partial(nn.modules.activation.LeakyReLU, negative_slope=0.1, inplace=True))
         print(f"Replacing first layer with new conv: {new_conv}")
         detection_model.backbone.module[0] = new_conv
-
+        return detection_model
     elif model_type=="rgbd":
-        detection_model = ln.models.YoloFusion(num_classes=3, fuse_layer=fuse_layer)
+        fusion_model = ln.models.YoloFusion(num_classes=3, fuse_layer=fuse_layer)
+        rgb_state_dict = torch.load("models/rgb_state_dict.pth")
+        renamed_rgb_state_dict = rename_state_dict(rgb_state_dict, remap_rgbd_15_fusion_model)
+        fusion_model.load_state_dict(renamed_rgb_state_dict, strict=False)
+
+        ## test equality of a single layer, must improve to test all
+        fusion_weight = fusion_model.state_dict()['layers.1.regular.11_convbatch.layers.1.weight'].cpu()
+        rgb_weight = rgb_state_dict['backbone.module.11_convbatch.layers.1.weight'].cpu()
+        if torch.equal(fusion_weight, rgb_weight):
+            print("Weights equivalent")
+        else: 
+            print("Weights not equivalent")
 
     
-    return detection_model
+        return fusion_model
